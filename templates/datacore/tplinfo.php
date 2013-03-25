51f501cf2020135266d905819abe991112ea447a
gongming <mg90@foxmail.com>
2013-03-25 15:16:28 +0800

    edit datacore templete

--- templates/datacore/tplinfo.php
+++ templates/datacore/tplinfo.php
@@ -1,6 +1,6 @@
 <?php
-$tpl_name        =    "默认模板";                //模板名称
-$tpl_thumb        =    "";                        //模板缩略图地址
-$tpl_author        =    "DataCore Team";                //模板作者
-$tpl_contact    =    "http://inet.hitwh.edu.cn";    //模板作者的联系方式
+$tpl_name = "DataCore模板";//模板名称
+$tpl_thumb = "";//模板缩略图地址
+$tpl_author = "DataCore Team";//模板作者
+$tpl_contact = "http://wrud.net";//模板作者的联系方式
 ?>
